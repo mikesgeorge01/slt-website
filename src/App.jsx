a0d13cd392f360a74fb008be60bc6b327240fd9f
mikesgeorge01 <mikesgeorge@hotmail.com>
2026-01-20 08:15:54 +0000

    Fix route for talent development strategy page

--- src/App.jsx
+++ src/App.jsx
@@ -390,7 +390,7 @@ export default function App() {
         path="/stories/leadership-that-inspires"
         element={<LeadershipThatInspires />}
       />
-      <Route path="/news/talent-development-strategy" element={<TalentDevelopmentStrategy />} />
+      <Route path="/stories/talent-development-strategy" element={<TalentDevelopmentStrategy />} />
     </Routes>
   );
 }
